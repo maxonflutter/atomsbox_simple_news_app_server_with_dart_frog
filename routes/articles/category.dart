2eb97144dd4b8ab313792181008161d112b0dd83
Massimo Del Pezzo <massimo.dpezzo@gmail.com>
2023-03-13 14:03:48 +0700

    Additional routes

--- routes/articles/category.dart
+++ routes/articles/category.dart
@@ -1,8 +1,27 @@
+import 'dart:io';
+
+import 'package:atomsbox_simple_news_app_server_with_dart_frog/src/news_data_source/news_data_source.dart';
 import 'package:dart_frog/dart_frog.dart';
 
-Response onRequest(RequestContext context) {
-  return Response(
-    body:
-        'This route returns a list of articles based on the category selected',
+Future<Response> onRequest(RequestContext context) async {
+  if (context.request.method != HttpMethod.get) {
+    return Response(statusCode: HttpStatus.methodNotAllowed);
+  }
+
+  final newsDataSource = context.read<NewsDataSource>();
+
+  final params = context.request.uri.queryParameters;
+  var newsCategory = params['newsCategory'];
+
+  final articles = await newsDataSource.getArticlesByCategory(
+    newsCategory: newsCategory ?? '',
+  );
+
+  if (articles.isEmpty) return Response(statusCode: HttpStatus.notFound);
+
+  return Response.json(
+    body: {
+      'results': articles.map((article) => article.toJson()).toList(),
+    },
   );
 }
--- routes/articles/popular.dart
+++ routes/articles/popular.dart
@@ -1,7 +1,21 @@
+import 'dart:io';
+
+import 'package:atomsbox_simple_news_app_server_with_dart_frog/src/news_data_source/news_data_source.dart';
 import 'package:dart_frog/dart_frog.dart';
 
-Response onRequest(RequestContext context) {
-  return Response(
-    body: 'This route returns a list of popular news articles',
+Future<Response> onRequest(RequestContext context) async {
+  if (context.request.method != HttpMethod.get) {
+    return Response(statusCode: HttpStatus.methodNotAllowed);
+  }
+
+  final newsDataSource = context.read<NewsDataSource>();
+  final articles = await newsDataSource.getBreakingNewsArticles();
+
+  if (articles.isEmpty) return Response(statusCode: HttpStatus.notFound);
+
+  return Response.json(
+    body: {
+      'results': articles.map((article) => article.toJson()).toList(),
+    },
   );
 }
