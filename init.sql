412b3082d018e8363384e2c0d381c3f08e709006
Massimo Del Pezzo <massimo.dpezzo@gmail.com>
2023-05-23 07:47:51 +0200

    Update to init

--- init.sql
+++ init.sql
@@ -15,13 +15,15 @@ CREATE TABLE articles (
     category VARCHAR(100) NOT NULL,
 );
 
-
 INSERT INTO authors (id, name, surname)
 VALUES ('1', 'Massimo', 'D.'),
        ('2', 'John', 'Smith'),
        ('3', 'Jane', 'Doe');
 
 INSERT INTO articles (id, author_id, headline, lead_paragraph, image_url, is_breaking_news, is_popular, category)
-VALUES ('1', '1', 'Headline 1', 'Lead paragraph 1', 'https://www.google.com', 'true', 'true', 'category 1'),
-       ('2', '2', 'Headline 2', 'Lead paragraph 2', 'https://www.google.com', 'false', 'false', 'category 2'),
-       ('3', '3', 'Headline 3', 'Lead paragraph 3', 'https://www.google.com', 'true', 'false', 'category 3');
+VALUES ('1', '1', 'Headline 1', 'This is article #1', 'https://www.atomsbox.com', 'true', 'true', 'category 1'),
+       ('2', '2', 'Headline 2', 'This is article #2', 'https://www.atomsbox.com', 'false', 'false', 'category 2'),
+       ('3', '3', 'Headline 3', 'This is article #3', 'https://www.atomsbox.com', 'true', 'false', 'category 3');
+       ('4', '1', 'Headline 4', 'This is article #4', 'https://www.atomsbox.com', 'false', 'true', 'category 4'),
+       ('5', '2', 'Headline 5', 'This is article #5', 'https://www.atomsbox.com', 'true', 'true', 'category 5'),
+       ('6', '3', 'Headline 6', 'This is article #6', 'https://www.atomsbox.com', 'false', 'false', 'category 6');
